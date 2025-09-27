be9016f61863257f52e7c0b92898576feeff31ad
Sharon Mushy <155962275+SMushy14@users.noreply.github.com>
2025-09-27 21:50:17 +0200

    Return user email instead of 'OK' on successful auth

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -75,7 +75,7 @@ def profile() -> str:
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
     if user:
-        return "OK", 200
+        return jsonify({"email": user.email}), 200
     else:
         abort(403)
 
